43b5474fda9e7472ef0b0147be8a4e6cbde9b205
chebupelka8 <stpzamyatin@gmail.com>
2025-11-28 14:05:55 +0500

    Init2

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+import json 
+
+
+app = FastAPI(
+    title="(._.)"
+)
+
+@app.get("/")
+async def root():
+    return {
+        "status": 200,
+        "message": "hello"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
