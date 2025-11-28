fcfdd33f3e97c9e722785bbea2a539bb0b606c11
chebupelka8 <stpzamyatin@gmail.com>
2025-11-28 14:17:46 +0500

    Init

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ async def root():
     }
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="localhost", port=8080, reload=True)
